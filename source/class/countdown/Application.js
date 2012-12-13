1ff2a00c37f0705d7846487e40db8fa5d940ec01
Marcos Lima <mlima.cpmbraxis@supervia.com.br>
2012-12-13 12:01:36 -0200

    O código foi otimizado, algumas variáveis se tornaram itens de um array e agora é tudo feito dentro de um loop for. Uma pequena animação está sendo feita para pesquisarmos as possibilidades.

--- source/class/countdown/Application.js
+++ source/class/countdown/Application.js
@@ -77,34 +77,26 @@ qx.Class.define("countdown.Application",
 			var fntHora = qx.bom.Font.fromString("150px bold Arial");
 			var cFlds = [];
 			var cLabels = ["Dias","Horas","Minutos","Segundos"];
-			var k;
-			for(k = 0; k < 2; k++)
-			{
-				cFlds[k] = [];
-				for(i = 0; i < 4; i++)
-				{
-					cFlds[k][i] = new qx.ui.basic.Label("00");
-					cFlds[k][i].set(
-					{
-						value: "00",
-						rich:false,
-						textAlign:"center"
-					});
-					
-					cFlds[k][i].setBackgroundColor("#202020");
-					cFlds[k][i].setTextColor("#ffffff");
-					cFlds[k][i].setAlignX("center");
-					cFlds[k][i].setFont(fntHora);
 
-					if(k == 0)
-					{
-						MainWnd.add(cFlds[k][i],{row:0,column:i});
-						var cLbl = new qx.ui.basic.Label(cLabels[i]);
-						cLbl.setTextColor("#ffff00");
-						cLbl.setTextAlign("center");
-						MainWnd.add(cLbl,{row:1,column:i});
-					}
-				}
+			for(i = 0; i < 4; i++)
+			{
+				cFlds[i] = new qx.ui.basic.Label("00");
+				cFlds[i].set(
+				{
+					rich:false,
+					textAlign:"center"
+				});
+				
+				cFlds[i].setBackgroundColor("#202020");
+				cFlds[i].setTextColor("#ffffff");
+				cFlds[i].setAlignX("center");
+				cFlds[i].setFont(fntHora);
+
+				MainWnd.add(cFlds[i],{row:0,column:i});
+				var cLbl = new qx.ui.basic.Label(cLabels[i]);
+				cLbl.setTextColor("#ffff00");
+				cLbl.setTextAlign("center");
+				MainWnd.add(cLbl,{row:1,column:i});
 			}
 
 			// Constantes auxiliares:
@@ -112,8 +104,25 @@ qx.Class.define("countdown.Application",
 			var HORAS   = MINUTOS * 60;
 			var DIAS    = HORAS   * 24;
 			
-			var frame = 0;
+			// NumberFormat para formatarmos com zeros à esquerda:
+			var nf = new qx.util.format.NumberFormat();
+			nf.setMinimumIntegerDigits(2);
 			
+			// Animação:
+			var duracao = 200;
+			var animation = 
+			{
+				duration : duracao, 
+				timing	 : "ease-out", 
+				origin   : "50% 50%",
+				keyFrames: 
+				{
+					  0 : {scale: [1,1], translate : ["0px", "0px"]},
+					 50 : {scale: [1,0], translate : ["0px", "0px"]},
+					100 : {scale: [1,1], translate : ["0px", "0px"]} 
+				}
+			};
+				  
 			// Ligamos o timer
 			var tmTimer = new qx.event.Timer(1000);
 			tmTimer.addListener("interval",function(e)
@@ -121,21 +130,35 @@ qx.Class.define("countdown.Application",
 				// Calculamos a diferença:
 				var dtNow = new Date();
 				var nEta = (dtFim.valueOf() - dtNow.valueOf())/1000; // segundos
+				var aFlds  = []; // Valor numérico do campo 
+				var aFmted = []; // String com o campo formatado
+				var aUnits = [DIAS,HORAS,MINUTOS,1];
+				var i;
 				
-				var Dias  = Math.floor(nEta / DIAS);
-				nEta %= DIAS;
-				var Horas = Math.floor(nEta / HORAS);
-				nEta %= HORAS;
-				var Minutos = Math.floor(nEta / MINUTOS);
-				var Segundos = Math.floor(nEta) % MINUTOS;
-				var nf = new qx.util.format.NumberFormat();
-				nf.setMinimumIntegerDigits(2);
+				for(i = 0; i < 4; i++)
+				{
+					aFlds[i] = Math.floor(nEta / aUnits[i]);
+					nEta %= aUnits[i];
+				}
 				
-				cFlds[frame][0].setValue(nf.format(Dias	));
-				cFlds[frame][1].setValue(nf.format(Horas	));
-				cFlds[frame][2].setValue(nf.format(Minutos	));
-				cFlds[frame][3].setValue(nf.format(Segundos));
-				
+				// Os diferentes deverão ser animados:
+				var oAnimate = [];
+					
+				// Agora tratamos cada campo de acordo com a mudança (se houver)
+				for(i = 0; i < 4; i++)
+				{
+					aFmted[i] = nf.format(aFlds[i]);
+					
+					
+					// Se houver diferença, fazemos a animação:
+					if(aFlds[i] != (1*cFlds[i].getValue()))
+					{
+						qx.bom.element.Animation.animate(cFlds[i].getContentElement().getDomElement(), animation);
+						cFlds[i].setValue(aFmted[i]);
+					}
+					
+					
+				}
 			},this);
 			
 			tmTimer.start();
